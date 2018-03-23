d3ff7b18ce4ea6f4c8f59590f035c4881fa2b31f
Babai <31babaev@gmail.com>
2018-03-23 13:06:27 +0300

    new2303

--- js/js.js
+++ js/js.js
@@ -1,46 +1,128 @@
-// var swidth=(window.innerWidth-$(window).width());
+$(function() {
 
-// $(function() {
+	$(window).resize(function(){
+		fixed_offset = $('.header__main').height();
+	});
 
-// 	var fixed_offset = $('.header__topline').height();
-
-// 	$(window).resize(function(){
-// 		fixed_offset = $('.header__topline').height();
-// 	});
-
-// 	$(".js-main-nav a, .js-botbtn").on("click", function (event) {
+	$(".js-catalog-link").on("click", function (event) {
 		
-// 		event.preventDefault();
-// 		var id = $(this).attr('href').split('#')[1], 
-// 			top = $('#' + id).offset().top;
+		event.preventDefault();
+		var id = $(this).attr('href').split('#')[1], 
+			top = $('#' + id).offset().top;
 		
-// 		$('body,html').animate({scrollTop: top - fixed_offset}, 1000);
-// 	});
+		$('body,html').animate({scrollTop: top}, 1000);
+	});
 
-// });
+});
 
-// $(function() {
+$(function() {
 
-// 	var $modalPopup = $('.js-popup-modal');
-// 	if(!$modalPopup.length) return;
+	var $modalPopup = $('.js-popup-modal');
+	if(!$modalPopup.length) return;
 
-// 	$modalPopup.magnificPopup({
-// 		type: 'inline',
-// 		preloader: false,
-// 		focus: '#username',
-// 		showCloseBtn: false,
-// 		mainClass: 'popup-modal-overlay',
-// 		callbacks: {
-// 			open: function() { $('.header__topline').css('padding-right', swidth + "px"); }, 
-// 			close: function() { $('.header__topline').css('padding-right', 0); },
-// 		}
-// 	});
+	$modalPopup.magnificPopup({
+		type: 'inline',
+		preloader: false,
+		focus: '#username',
+		showCloseBtn: false,
+		mainClass: 'popup-modal-overlay'
+	});
 
-// 	$(document).on('click', '.popup-modal-dismiss', function (e) {
-// 		e.preventDefault();
-// 		$.magnificPopup.close();
-// 	});
-// });
+	$(document).on('click', '.popup-modal-dismiss', function (e) {
+		e.preventDefault();
+		$.magnificPopup.close();
+	});
+});
+
+$(function() {
+
+	$('.js-form-phone').each(function(){
+		// Объявляем переменные (форма и кнопка отправки)
+		var form = $(this),
+		    btn = form.find('.phone-btn');
+
+		// Добавляем каждому проверяемому полю, указание что поле пустое
+		form.find('.rfield').addClass('empty_field');
+
+		// Функция проверки полей формы
+		function checkInput(){
+			form.find('.rfield').each(function(){
+				if($(this).val() != ''){
+					  // Если поле не пустое удаляем класс-указание
+					$(this).removeClass('empty_field');
+				} else {
+					  // Если поле пустое добавляем класс-указание
+					$(this).addClass('empty_field');
+				}
+			});
+		}
+
+		// Функция подсветки незаполненных полей
+		function lightEmpty(){
+			form.find('.empty_field').addClass('error');
+			// Через полсекунды удаляем подсветку
+			setTimeout(function(){
+			form.find('.empty_field').removeClass('error');
+			},500);
+		}
+
+		// Проверка в режиме реального времени
+		setInterval(function(){
+			// Запускаем функцию проверки полей на заполненность
+			checkInput();
+			// Считаем к-во незаполненных полей
+			var sizeEmpty = form.find('.empty_field').length;
+			// Вешаем условие-тригер на кнопку отправки формы
+			if(sizeEmpty > 0){
+			if(btn.hasClass('disabled')){
+			  return false
+			} else {
+				btn.addClass('disabled')
+			}
+			} else {
+				btn.removeClass('disabled')
+			}
+		}, 500);
+
+
+		// Событие клика по кнопке отправить
+		btn.click(function(){
+		  if($(this).hasClass('disabled')){
+		    // подсвечиваем незаполненные поля и форму не отправляем, если есть незаполненные поля
+			lightEmpty();
+			return false
+		  } else {
+				form.submit(function (){
+
+					var data = form.serialize();
+
+					$.ajax({ 
+						// инициaлизируeм ajax зaпрoс
+						type: 'POST', // oтпрaвляeм в POST фoрмaтe, мoжнo GET
+						url: 'mail.php', // путь дo oбрaбoтчикa, у нaс oн лeжит в тoй жe пaпкe
+						data: data, // дaнныe для oтпрaвки
+						error: function(xhr, status, error) {
+							console.log(xhr.responseText + '|\n' + status + '|\n' +error);
+						},
+						success: function(data){ // сoбытиe пoслe удaчнoгo oбрaщeния к сeрвeру и пoлучeния oтвeтa
+							form[0].reset();
+							$.magnificPopup.close();
+							$.magnificPopup.open({
+								showCloseBtn: false,
+								items: {
+									src: '#after-modal'
+								},
+								type: 'inline'
+								}, 
+							0);
+						}
+					});
+					return false;
+				});
+			}
+		});
+	});
+});
 
 // $(function() {
 
@@ -63,27 +145,4 @@
 // 		0);
 // 		return false;
 // 	});
-// });
-
-// $(function() {
-
-// 	var $contactMap = $('.js-contact-map');
-// 	if(!$contactMap.length) return;
-
-// 	var map,
-// 		center = {lat: 55.873569, lng: 37.458601},
-// 		point = {lat: 55.874022, lng: 37.467957};
-
-// 	(function initMap() {
-// 		map = new google.maps.Map(document.getElementById('contact-map'), {
-// 		center: center,
-// 		zoom: 15,
-// 		scrollwheel: true
-// 		});
-// 		var marker = new google.maps.Marker({
-// 			position: point,
-// 			map: map
-// 		});
-// 	})();
-
 // });
---
+++ mail.php
@@ -0,0 +1,18 @@
+<?
+if((isset($_POST['phone'])&&$_POST['phone']!="")){ //Проверка отправилось ли наше поля name и не пустые ли они
+        $to = 'prasolova20@gmail.com'; //Почта получателя, через запятую можно указать сколько угодно адресов
+        $subject = 'Заказ обратного звонка'; //Заголовок сообщения
+        $message = '
+                <html>
+                    <head>
+                        <title>'.$subject.'</title>
+                    </head>
+                    <body>
+                        <p>Телефон: <b>'.$_POST['phone'].'</b></p>                     
+                    </body>
+                </html>'; //Текст нащего сообщения можно использовать HTML теги
+        $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
+        $headers .= "From: Doodoo <prasolova20@gmail.com>\r\n"; //Наименование и почта отправителя
+        mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
+}
+?>
